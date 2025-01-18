d51daa0cde744d88a0ecec483b11bc60bb742cf1
moley73 <briandarrington73@gmail.com>
2025-01-18 14:43:04 +0000

    fix: update configuration for better Node.js 18 compatibility

--- next.config.js
+++ next.config.js
@@ -10,10 +10,14 @@ const nextConfig = {
     loaderFile: './image-loader.js',
     path: '/',
     remotePatterns: [],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   basePath: basePath,
   assetPrefix: basePath,
   trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
