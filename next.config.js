e1ea0c1f881f155aa895a8483852b45489f2f953
moley73 <briandarrington73@gmail.com>
2025-01-18 13:53:11 +0000

    Add GitHub Pages deployment configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/sadrc-website',
+  assetPrefix: '/sadrc-website/',
+}
+
+module.exports = nextConfig
