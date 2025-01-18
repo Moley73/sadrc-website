37c21cdf239b9cbc23bef071a230a8ce18ed29f6
moley73 <briandarrington73@gmail.com>
2025-01-18 15:02:58 +0000

    chore: add netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/sadrc-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/sadrc-website/' : '',
   images: {
     unoptimized: true,
   },
